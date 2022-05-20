d98883890aea46355910902526dcc6698e65e8d3
MDimas27 <mdimas.umb@gmail.com>
2022-05-20 16:31:45 +0700

    Quiz 3 - hasil nilai rata-rata dengan komanya

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 
 	// hitung rata2
 	// scores := [8]int{100, 80, 75, 92, 70, 93, 88, 67}
+	// hasil harus beserta komanya
 	scores := [8]int{100, 80, 75, 92, 70, 93, 88, 67}
 
 	var total int
@@ -14,7 +15,7 @@ func main() {
 	}
 
 	length := len(scores)
-	average := total / length
+	average := float64(total) / float64(length)
 
 	fmt.Println(average)
 
