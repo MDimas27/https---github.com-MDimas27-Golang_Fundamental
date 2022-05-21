34712867ef45cdf6f617211e205f28c5efbcc93e
MDimas27 <mdimas.umb@gmail.com>
2022-05-21 17:11:14 +0700

    Membuat function operasi matematika

--- main.go
+++ main.go
@@ -1,23 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 
-	// hitung rata2
-	// scores := [8]int{100, 80, 75, 92, 70, 93, 88, 67}
-	// hasil harus beserta komanya
-	scores := [8]int{100, 80, 75, 92, 70, 93, 88, 67}
+	//result, err := calculate(10, 2, "+")
+	//result, err := calculate(10, 2, "-")
+	//result, err := calculate(10, 2, "*")
+	//result, err := calculate(10, 2, "/")
+	//result, err := calculate(10, 2, "=")
 
-	var total int
-	for _, score := range(scores) {
-		total = total + score
+	result, err := calculate(10, 2, "m")
+	if err != nil {
+		fmt.Println("Terjadi kesalahan")
+		fmt.Println(err.Error())
 	}
 
-	length := len(scores)
-	average := float64(total) / float64(length)
-
-	fmt.Println(average)
+	fmt.Println(result)
 
 
+}
+
+func calculate(number, numberTwo int, operation string) (int, error) {
+	var result int
+	var errorResult error
+
+	switch operation {
+	case "+":
+		result = number + numberTwo
+	case "-":
+		result = number - numberTwo
+	case "*":
+		result = number * numberTwo
+	case "/":
+		result = number / numberTwo
+	default:
+		errorResult = errors.New("Operasi Tidak dikenali")
+	}
+
+	return result, errorResult
 }
