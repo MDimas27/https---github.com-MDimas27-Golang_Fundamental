60f67d662c96767079012998bd5d078903df5167
MDimas27 <mdimas.umb@gmail.com>
2022-05-30 08:30:23 +0700

    quiz 5 - Membuat method

--- main.go
+++ main.go
@@ -2,20 +2,74 @@ package main
 
 import "fmt"
 
-func main() {
+type User struct {
+	ID				int
+	FirstName		string
+	LastName		string
+	Email			string
+	isActive		bool
 
-	// hitung rata2
-	// scores := [8]int{100, 80, 75, 92, 70, 93, 88, 67}
-	// hasil harus beserta komanya
-	scores := [8]int{100, 80, 75, 92, 70, 93, 88, 67}
+} 
 
-	var total int
-	for _, score := range(scores) {
-		total = total + score
+func (user User) display() string {
+	return fmt.Sprintf("Name : %s %s, Email : %s", user.FirstName, user.LastName, user.Email)
+}
+
+func displayUser(user User) string {
+	result := fmt.Sprintf("Name : %s %s, Email : %s", user.FirstName, user.LastName, user.Email)
+	return result
+}
+
+type Group struct {
+	Name		string
+	Admin		User
+	Users		[]User
+	IsAvailable bool
+}
+
+func (group Group) DisplayGroup() {
+	fmt.Printf("Name : %s", group.Name)
+	fmt.Println("")
+	fmt.Printf("Member count : %d", len(group.Users))
+	fmt.Println("")
+
+	fmt.Println("Users name :")
+	for _, user := range group.Users {
+		fmt.Println(user.FirstName)
+	}
+}
+
+func displayGroup(group Group) {
+	fmt.Printf("Name : %s", group.Name)
+	fmt.Println("")
+	fmt.Printf("Member count : %d", len(group.Users))
+	fmt.Println("")
+
+	fmt.Println("Users name :")
+	for _, user := range group.Users {
+		fmt.Println(user.FirstName)
 	}
 
-	length := len(scores)
-	average := float64(total) / float64(length)
-
-	fmt.Println(average)
 }
+
+
+func main() {
+
+	user := User{1, "Dimas", "Surya","dimas@email.com", true }
+	result := user.display()
+	fmt.Println(result)
+
+	user2 := User{2, "Dede", "Nugraha","surya@email.com", true }
+	fmt.Println(user2.display())
+
+	users := []User{user, user2}
+	group := Group{"Gamer", user, users, true}
+
+	// Quiz/5 Membuat function menjadi Method
+	// displayGroup(group)
+	group.DisplayGroup()
+
+}
+
+
+
